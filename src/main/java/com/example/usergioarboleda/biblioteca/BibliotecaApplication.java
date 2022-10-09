a052fa8293ad8ef4e02eb49e34c2855a5088cd76
casierrav-sa <carlos.sierra1.mt@usa.edu.co>
2022-10-09 15:04:37 -0500

    Application main file

---
+++ src/main/java/com/example/usergioarboleda/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,12 @@
+package com.example.usergioarboleda.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
